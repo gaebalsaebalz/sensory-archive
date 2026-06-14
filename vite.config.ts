6045c765a016319e49079920cae9eb8c6bbfe53e
noah <d0hamzie@gmail.com>
2026-06-15 00:50:55 +0900

    fix env variables for cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,22 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
-
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
+  define: {
+    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
+    'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY),
+  },
   plugins: [
     devtools(),
-    nitro({ rollupConfig: { external: [/^@sentry\//] } }),
+    nitro({ 
+      preset: 'cloudflare-pages',
+      rollupConfig: { external: [/^@sentry\//] } 
+    }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
