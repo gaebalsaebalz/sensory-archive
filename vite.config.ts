15ff559ef717e389413b34314ddabc34a5c49366
noah <d0hamzie@gmail.com>
2026-06-15 17:15:13 +0900

    hardcode env for cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -8,15 +8,15 @@ import { nitro } from 'nitro/vite'
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   define: {
-    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
-    'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY),
+    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify('https://lbgugcnumubgfdmkrhft.supabase.co'),
+    'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxiZ3VnY251bXViZ2ZkbWtyaGZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE0Mzc2MjYsImV4cCI6MjA5NzAxMzYyNn0.rkn1vnH5PtqKE4uM2NGyD8F3FfguZlj6GkgiaXRd5XA'),
   },
   plugins: [
     devtools(),
     nitro({ 
       preset: 'cloudflare_module',
       rollupConfig: { external: [/^@sentry\//] } 
-}),
+    }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
